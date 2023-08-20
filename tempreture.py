fb88d98df52c29da91dd6c1dcad23dbf217c5176
KrunalRana12 <142747334+KrunalRana12@users.noreply.github.com>
2023-08-21 01:04:43 +0530

    file manger

---
+++ tempreture.py
@@ -0,0 +1,9 @@
+celsius=float(input("enter the tempreture of celsius = "))
+fahrenheit=(celsius*9/5)+32
+
+print("---- the celsius to fahrenheit is --- :- ",fahrenheit)
+
+fahrenheit=float(input("enter the tempreture of fahrenheit = "))
+celsius=((fahrenheit-32)*5)/9
+
+print("---- the fahrenheit to celsius is --- :- ",celsius)
